f419dd957d6fa614db629f51d48fa84811bb8071
DHARUN1819 <115345519+DHARUN1819@users.noreply.github.com>
2024-01-25 22:07:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ import productRoutes from "./routes/productRoutes.js";
 import prescriptionRoutes from './routes/prescriptionRoutes.js';
 import cors from "cors";
 import path from 'path';
+import { fileURLToPath } from 'url';
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+const __filename=fileURLToPath( import.meta.url); 
+const __dirname=path.dirname(__filename);
 
 //rest object
 const app = express();
