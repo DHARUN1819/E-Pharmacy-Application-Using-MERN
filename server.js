d4a45aaf0832d59c7d6a96e4d752e856e97d80f1
DHARUN1819 <dharundeepak1819@gmail.com>
2024-01-28 09:55:35 +0530

    updated

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import prescriptionRoutes from './routes/prescriptionRoutes.js';
 import cors from "cors";
+import {fileURLToPath } from 'url';
+
 import path from 'path';
 //configure env
 dotenv.config();
@@ -15,14 +17,19 @@ dotenv.config();
 //databse config
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname =path.dirname(__filename);
+
 //rest object
 const app = express();
+ I
+ 
 
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(__dirname,'./client/build'))
+app.use(express.static(path.join(__dirname,'./client/build'))) 
 
 //routes
 app.use("/api/v1/auth", authRoutes);
